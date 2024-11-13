f4644c5ed8f1a7340bba12a26489edad874253ae
Amr Khimes <amrkhmes343@gmail.com>
2024-11-13 13:29:14 +0200

    finsh Notifications

---
+++ lib/FCM/fcm.dart
@@ -0,0 +1,83 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
+
+class Fcm {
+  static FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  static Future<void> iosParmition() async {
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    print('User granted permission: ${settings.authorizationStatus}');
+  }
+
+  static Future<void> fcmInti() async {
+    print(getToken());
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+    await iosParmition();
+    await getToken();
+    await handelMessageForground();
+  }
+
+  static Future<void> handelMessageForground() async {
+    const AndroidNotificationChannel channel = AndroidNotificationChannel(
+      'high_importance_channel', // id
+      'High Importance Notifications', // title
+      description:
+          'This channel is used for important notifications.', // description
+      importance: Importance.max,
+    );
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+
+      // If `onMessage` is triggered with a notification, construct our own
+      // local notification to show to users using the created channel.
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                channelDescription: channel.description,
+                icon: "@mipmap/ic_launcher",
+                // other properties...
+              ),
+            ));
+      }
+    });
+  }
+
+  static Future<String?> getToken() async {
+    String? token = await messaging.getToken();
+    print('FCM token: $token');
+    return token;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/App_theme.dart';
+import 'package:flutter_application_1/FCM/fcm.dart';
 import 'package:flutter_application_1/auth/log_in.dart';
 import 'package:flutter_application_1/auth/registretion.dart';
 import 'package:flutter_application_1/taps/home_screen.dart';
@@ -10,9 +11,11 @@ import 'package:provider/provider.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(ChangeNotifierProvider(
-    create:(_)=>TasksProvider(),
-    child: todoApp()));
+ await Fcm.fcmInti();
+
+
+  runApp(
+      ChangeNotifierProvider(create: (_) => TasksProvider(), child: todoApp()));
 }
 
 class todoApp extends StatelessWidget {
@@ -21,10 +24,10 @@ class todoApp extends StatelessWidget {
     return MaterialApp(
       title: 'Todo App',
       debugShowCheckedModeBanner: false,
-      routes: {HomeScreen.route: (_) => HomeScreen(),
-      LogIn.route:(_)=>LogIn(),
-      Registretion.route:(_)=>Registretion(),
-      
+      routes: {
+        HomeScreen.route: (_) => HomeScreen(),
+        LogIn.route: (_) => LogIn(),
+        Registretion.route: (_) => Registretion(),
       },
       initialRoute: HomeScreen.route,
       theme: AppTheme.lightTheme,
